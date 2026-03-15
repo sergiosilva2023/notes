e5efa099e9c0ae377db7fbc6c19e19b265ec6454
Sérgio <sergio.web100@gmail.com>
2026-03-15 09:25:22 +0000

    loginSubmit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,12 +7,23 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     // metodo para login
-    public function login (){
-        echo 'Login';
+    public function login ()
+    {
+        return view('login');
+    }
+
+    // metodo loginSubmit
+    public function loginSubmit (Request $request)
+    {
+        echo $request->input('text_username');
+        echo '<br/>';
+        echo $request->input('text_password');
     }
 
     // metodo para logout
-    public function logout (){
+    public function logout ()
+    {
         echo 'Logout';
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 // auth routes
 Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 Route::get('/logout', [AuthController::class, 'logout']);
 
 Route::get('/', function (){
